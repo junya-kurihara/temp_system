f390ea417c670a46115fe0b2efadb092bc0b7c25
junya.kurihara <junya.kurihara@town.shisui.chiba.jp>
2022-08-23 16:40:54 +0900

    controll クラス ・main関数を修正

--- controller/controller.py
+++ controller/controller.py
@@ -1,5 +1,7 @@
 import AES256
+import os
 from model import model
+from view import view
 
 
 def encrypt_temper(exec_date, temper, key):
@@ -8,14 +10,22 @@ def encrypt_temper(exec_date, temper, key):
     return encrypted
 
 
-#def main():
-#    temper = model.model.get_temperature()
-#    exec_date = model.model.get_exec_datetime()
+def main():
+    m = model.Model()
+    temper = m.get_temperature()
+    exec_date = m.get_exec_datetime()
+
+    encrypted = encrypt_temper(exec_date, temper, '12345')
+
+    output_file = '/home/kurihara/work/202205_temp_sys/temp_sys/data/data.dat'
+    db_path = os.getcwd() + sep + 'db' + sep + 'temp_test.db'
+    view.save_encrypted_file(encrypted, output_file)
+
+    m.init_db()
+    m.temp_create_db()
+    m.insert_temper_db(exec_date, exec_date, temper)
+    m.select_db()
 
 
 if __name__ == '__main__':
-    temper = model.Model().get_temperature()
-    exec_date = model.Model().get_exec_datetime()
-    encrypted = encrypt_temper(exec_date, temper, '12345')
-    print(encrypted)
-
+    main()
